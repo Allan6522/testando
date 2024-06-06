ffef9d02175f2794609caae6592eb122197228db
renato.palitinho1995@gmail.com <renato.palitinho1995@gmail.com>
2024-06-05 21:59:10 -0300

    testando

---
+++ impar_par.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+main(){
+	
+	int a;
+	
+	printf("entre co  um numero \n");
+	
+	scanf("%d",&a);
+	if(a % 2 == 0)
+	
+	printf("\n\n %d e par \n\n",a);
+	
+	else printf("\n %d e impar \n\n",a);
+	system ("pause");
+}
